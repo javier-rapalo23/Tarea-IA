9170e70e724b6823fbdc89445beaccedac5b545e
javier-rapalo23 <javierorellana2015.jaor@gmail.com>
2023-03-27 23:36:21 -0600

    final

--- js/index.js
+++ js/index.js
@@ -1,31 +1,34 @@
 const btn = document.getElementById('btn')
 
-const convertir = async (path, valor) => {
+const enviar = async (path, valor) => {
   const modelo = await tf.loadLayersModel(path)
   const tensor = tf.tensor([valor])
   const result = modelo.predict(tensor)
-  const resultadoFinal = result.dataSync()[0]
+  const resultado = result.dataSync()[0]
   tensor.dispose()
   result.dispose()
 
   document.getElementById('result').innerHTML =
-    Math.round(resultadoFinal * 100) / 100
+    Math.round(resultado * 100) / 100
 }
 
 btn.addEventListener('click', (event) => {
   event.preventDefault()
   const valor = Number(document.getElementById('valor').value)
   const o = document.getElementById('option').value
-
+  
   switch (o) {
     case '0':
-      convertir('models/Neurona1_M_a_C/model.json', valor)
+      enviar('models/Neurona1_M_a_C/model.json', valor)
       break
     case '1':
-      convertir('models/Neurona2_K_a_L/model.json', valor)
+      enviar('models/Neurona2_K_a_L/model.json', valor)
 
       break
     case '2':
-      convertir('models/Neurona3_C_a_F/model.json', valor)
+      enviar('models/Neurona3_C_a_F/model.json', valor)
+      break
+    default:
+      console.log('Error')
   }
 })
